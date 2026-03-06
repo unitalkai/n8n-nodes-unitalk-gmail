a043e2e881abb8caf674ea934aae1df6f8933458
Atouch Mohamed <4213033+atouchsh@users.noreply.github.com>
2026-03-06 01:29:35 +0000

    Add proxy

--- dist/nodes/Gmail/utils/replyToEmail.d.ts
+++
@@ -1,3 +0,0 @@
-import { type IDataObject, type IExecuteFunctions } from 'n8n-workflow';
-import type { GmailMessage } from '../types';
-export declare function replyToEmail(this: IExecuteFunctions, gmailId: string, options: IDataObject, itemIndex: number): Promise<GmailMessage>;
--- dist/nodes/Gmail/v2/utils/draft.d.ts
+++
@@ -1,2 +0,0 @@
-import type { IExecuteFunctions } from 'n8n-workflow';
-export declare function addThreadHeadersToEmail(this: IExecuteFunctions, email: any, threadId: string): Promise<void>;
